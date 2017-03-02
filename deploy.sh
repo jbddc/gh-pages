20918ffbad3639e52dd44e5435e137db5431c51d
Joao Bernardo Costa <jbernardoddc@gmail.com>
2017-03-02 20:55:18 +0000

    PROBLEM: No automation in website publishing
    SOLUTION: Shell script created to automate this task.

---
+++ deploy.sh
@@ -0,0 +1,34 @@
+# Temporarily store uncommited changes
+git stash
+
+# Verify correct branch
+git checkout dev
+
+# Build new files
+stack exec site clean
+stack exec site build
+
+# Get previous files
+git fetch --all
+git checkout -b master --track origin/master
+
+# Overwrite existing files with new files
+rsync -a --filter='P _site/'      \
+         --filter='P _cache/'     \
+         --filter='P .git/'       \
+         --filter='P .gitignore'  \
+         --filter='P .stack-work' \
+         --delete-excluded        \
+         _site/ .
+
+# Commit
+git add -A
+git commit -m "Publish."
+
+# Push
+git push origin master:master
+
+# Restoration
+git checkout dev
+git branch -D master
+git stash pop
